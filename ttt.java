2faea54caca38e8d904aea4adef207f458f60d44
JiuShu <zsz17682186994@163.com>
2022-11-01 17:53:34 +0800

    master test

--- ttt.java
+++ ttt.java
@@ -4,5 +4,7 @@ public class ttt {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello dev");
+        System.out.println("hello master");
+
     }
 }
